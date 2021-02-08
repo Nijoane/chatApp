f87f0c9df7e13add9cd521c34444607423f30156
Nijoane <nicajoane@outlook.com>
2021-02-08 01:46:55 -0300

    [chatComponent] add avatar to user message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -6,6 +6,19 @@ const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
 
   const chat = chats && chats[activeChat];
+
+  const renderReadReceipts = (message, isMyMessage) => {
+    return chat.people.map((person, index) => person.last_read === message.id && (
+      <div 
+        key={`read_${index}`}
+        className='read-receipt'
+        style={{
+          float: isMyMessage ? 'right' : 'left',
+          backgroundImage: `url(${person.person.avatar})`
+        }}
+      />
+    ))
+  }
   
   const renderMessages = () => {
     const keys = Object.keys(messages);
@@ -28,7 +41,7 @@ const ChatFeed = (props) => {
             className='read-receipts' 
             style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}
           > 
-            read-receipts
+            {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       );
